16207f5f915bab025a13559671c7a6b4e8055526
Davronbek <110102479+Gofurjonov@users.noreply.github.com>
2025-08-01 10:49:37 +0500

    lesson5

---
+++ lesson5.py
@@ -0,0 +1,36 @@
+# bozorlik = []
+# chiqarish = int(input("Nechta mahsulot hoxlaysiz? "))
+# for bozor in range(chiqarish):
+#     bozorlik.append(input('nima? '))
+# print(bozorlik )
+# for if data tipelar boyicha mavzular
+
+#task1
+# ismlar = ["Samir", "A'loxon", "Siroj", "Jasur", "Salox"]
+# for ism in ismlar:
+#     print("Assalomu aleykum", ism)
+
+#task2
+# ismlar = ["Samir", "A'loxon", "Siroj", "Jasur", "Salox"]
+# for ism in ismlar:
+#     print("Assalomu aleykum", ism)
+# print(f"Kod {len(ismlar)} marta takrorlandi")
+
+#task3
+# for son in range(11,100, 2):
+#     print(son**3)
+
+#task4
+# kinolar = []
+# users = print("5 ta eng sevimli kinolaringizni kiriting")
+# for user in range(5):
+#     kinolar.append(input(f"{user+1}-- kino:"))
+# print(kinolar)
+
+#task5
+# suhbat = int(input("Bugun necha kishi bilan suhbat qildingiz? "))
+# royhat = []
+# for k in range(suhbat):
+#     royhat.append(input(f"{k+1}--suhbat qilgan odamingiz kim edi: "))
+# print(royhat)
+
