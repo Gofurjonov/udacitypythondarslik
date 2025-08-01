7fd624ba5f1a669058d0878af90a3971a824e12d
Davronbek <110102479+Gofurjonov@users.noreply.github.com>
2025-08-01 09:58:09 +0500

    python

---
+++ lesson1.py
@@ -0,0 +1,61 @@
+# task 1
+# dostlar = ['Asadbek', 'Dilmurod',  'Amirbek']
+# print(f"Salom {dostlar[0]}, bugun choyxona bormi")
+# print(f"{dostlar[1]}, choyxonaga boramizmi")
+# print(f"Choyxonadan keyin birga qaytamizmi {dostlar[2]}")
+
+# task 2
+# sonlar = [27, -7, 2.14, 0, 200, -2.5]
+# sonlar[0] = sonlar[0] + 5
+# sonlar[1] = sonlar[1] * 2
+# sonlar[2] = sonlar[2] / 2
+# sonlar[3] = sonlar[3] - 4
+# sonlar[4] = sonlar[4] ** 2
+
+# print("yangilangan sonlar : ", sonlar)
+
+#task 3
+# t_shaxslar = ["Amir Temur", "Mirzo Ulug'bek", "Jaloliddin Manguberdi"]
+# z_shaxslar = ["Elon Musk", "Stiv Jobs", "Mark Zukerberg"]
+
+# t_tanlangan = t_shaxslar.pop(0)
+# z_tanlangan = z_shaxslar.pop(2)
+
+# print(f"Men tarixiy shaxlardan {t_tanlangan} bilan,")
+# print(f"zamonaviy shaxlardan esa {z_tanlangan} bilan")
+# print("suhbat qilishni istardim")
+
+#task 4
+# friends = []
+# friends.append("Samir")
+# friends.append("A'loxon")
+# friends.append("Saloxiddin")
+# friends.append("Siroj")
+# friends.append("Nurmuhammad")
+# friends.append("Ziyovuddin")
+
+# print(friends)
+#
+# friends.remove("Siroj")
+# friends.remove("Ziyovuddin")
+# print(friends)
+#
+# friends.insert(0, "Jamshid")
+# friends.insert(len(friends)//2, "Sardor")
+# friends.append('Abbos')
+
+# print("yangi insonlar ro'yxati: ", friends)
+#
+# mehmonlar = []
+# mehmonlar.append(friends.pop(0))
+# mehmonlar.append(friends.pop(3))
+# mehmonlar.append(friends.pop(1))
+#
+# print("kelgan mehmonlarning ro'yhati: ", mehmonlar)
+# print("qolgan dostlar royxati: ", friends)
+
+# listlar ustida ammallar bajariladi
+
+# royxatlarni tartiblash
+
+sonlar = [12, 3, 4, 9, 2, 1]
