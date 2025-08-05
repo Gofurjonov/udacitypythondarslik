fa16474475eb8ad8b1e020d04f1a77439852405e
Davronbek <110102479+Gofurjonov@users.noreply.github.com>
2025-08-06 00:01:38 +0500

    lesson 9

---
+++ lesson9.py
@@ -0,0 +1,22 @@
+# task 1,2 for
+# sonlar = [1, 2, 7, 10, 8]
+# eng_kattasi = sonlar[0]
+# for son in sonlar:
+#     if son > eng_kattasi:    #eng kattasi
+#         eng_kattasi = son
+# print(eng_kattasi)
+
+
+# sonlar = [1, 2, 7, 10, 8]
+# yigindi = 0
+# for son in sonlar:    #yigindi
+#     yigindi += son
+# print(yigindi)
+
+
+
+# for i in range(1, 10):  # 1 dan 9 gacha
+#     for j in range(1, 10):  # 1 dan 9 gacha
+#         print(f"{i} x {j} = {i*j}")
+#     print()  # Qatorni tashlash
+
