f4e58de9da5acdb07a7ef1ccec88dbee7d65bd35
Davronbek <110102479+Gofurjonov@users.noreply.github.com>
2025-08-10 20:15:01 +0500

    lesson14

--- lesson14.py
+++ lesson14.py
@@ -1,47 +1,15 @@
-# lambda YOHUD NOMSIZ FUNKSIYA
-# import math
-# uzunlik = lambda pi, r : 2*pi*r
-# print(uzunlik(math.pi,10))
-#
-# kvadrat = lambda x, y : x ** y
-# print(kvadrat(3, 2))
+month = 1
+paid_months = 0
+while month <= 12:
+    status = input(f"{month}-oy ijarani to‘ladingizmi? (ha/yo‘q): ")
+    try:
+        if status.lower() not in ["ha", "yo'q"]:
+            raise ValueError("Noto‘g‘ri javob! Faqat 'ha' yoki 'yo‘q' kiriting.")
+    except ValueError as e:
+        print(e)
+        continue
+    if status.lower() == "ha":
+        paid_months +=1
+    month +=1
 
-# def daraja(n):
-#     return lambda x : x**n
-# kvadrat = daraja(2)
-# kub = daraja(3)
-# print(f"3-ning kvadrati {kvadrat(3)} ga,"
-#       f"kubi {kub(3)} ga teng")
-
-from math import sqrt
-# sonlar = list(range(11))
-# ildizlar = list(map(sqrt, sonlar))
-# print(ildizlar)
-
-# def daraja2(x):
-#     return x*x
-# print(list(map(daraja2, sonlar)))
-
-# kvadrat = list(map(lambda x: x*x, sonlar))
-# print(kvadrat)
-
-# a = [4, 5, 6]
-# b = [7, 8, 9]
-# a_plus_b = list(map(lambda x, y: x+y, a, b))
-# print(a_plus_b)
-
-import random as r
-# sonlar = r.sample(range(100), 10)
-# # print(sonlar)
-# def juftmi(x):
-#     return x%2==0
-# juft_sonlar = list(filter(juftmi,sonlar))
-# print(juft_sonlar)
-
-# juft_sonlar = list(filter(lambda son: son%2 == 0, sonlar))
-# print(juft_sonlar)
-
-# mevalar = ['olma','anor','anjir','shaftoli',"o'rik","tarvuz","qovun","banan"]
-#
-# mevalar_b = list(filter(lambda meva:meva.startswith('b'),mevalar))
-# print(mevalar_b)
+print(f"Siz {paid_months} oy ijarani to‘ladingiz.")
