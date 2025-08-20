3064cfc760aeee49f9bf04aed5f59a9d517874ba
Davronbek <110102479+Gofurjonov@users.noreply.github.com>
2025-08-20 21:03:32 +0500

    lesson19

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-# import lesson13modul
-# avto1 = lesson13modul.avto_info("Gm", "Malibu", "Qora", "avtomat", 2025, 38000)
-# lesson13modul.info_print(avto1)
+from lesson19.task import yangilash,chiqarish,productlar,hisobot
 
-# import lesson13modul as l13mod
-# avto1 = l13mod.avto_info("Gm", "Malibu", "Qora", "avtomat", 2025, 38000)
-# l13mod.info_print(avto1)
 
-# from lesson13modul import avto_info, info_print
-# avto1 = avto_info("Gm", "Malibu", "oq", "avtomat", 2025, 38000)
-# info_print(avto1)
+def main():
+    while True:
+        try:
+            kiriysh = int(
+                input("(1).kiritish | (2).chiqarish | (3).productlar | (4).hisobot | (5).help | (0).chiqish >>> "))
+        except ValueError:
+            print("❌ Faqat 1, 2, 3, 4, 5 yoki 0 ni ishlata olasiz")
+            continue  # noto'g'ri kiritilganda sikl qaytadan boshlansin
 
-# from lesson13modul import avto_info as ainfo, info_print as iprint
-# avto1 =ainfo("Gm", "Malibu", "Qora", "avtomat", 2025, 38000)
-# iprint(avto1)
 
-# from lesson13modul import *
-# avto1 = avto_info("Gm", "Malibu", "Qora", "avtomat", 2025, 38000)
-# info_print(avto1)
 
-# import math
-# x=81
-# print(math.sqrt(x))
-# print(math.pow(5,3))
-# print(math.pi)
-# print(math.log2(8))
-# print(math.log10(100))
+        if kiriysh == 1:
+            yangilash()
+        elif kiriysh == 2:
+            chiqarish()
+        elif kiriysh == 3:
+            productlar()
+        elif kiriysh == 4:
+            hisobot()
+        elif kiriysh == 5:
+            print("1 → Omborga mahsulot kiritish\n"
+                  "2 → Ombordan chiqarish (sotish)\n"
+                  "3 → Hamma mahsulotni ko‘rish\n"
+                  "4 → Sana bo‘yicha hisobot\n"
+                  "0 → Dasturdan chiqish")
+        elif kiriysh == 0:
+            print("✅ Code to‘xtadi")
+            break
+        else:
+            print("❌ Faqat 1, 2, 3, 4, 5 yoki 0 ni tanlang")
 
-# import  random as r
-# son = r.randint(0,100)
-# print(son)
-
-import random as r
-ismlar = ["olim", "jamshid", "Mirzo", "shoxrux"]
-ism = r.choice(ismlar)
-print(ism)
+if __name__ == '__main__':
+    main()
